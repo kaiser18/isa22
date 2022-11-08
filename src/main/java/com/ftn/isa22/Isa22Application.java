6ed8b5789c0a0fac64f2315cbb1b0a6ad5dd6338
Mihailo Ivić <oliahim.ivic@gmail.com>
2022-11-08 13:10:19 +0100

    initial commit

---
+++ src/main/java/com/ftn/isa22/Isa22Application.java
@@ -0,0 +1,13 @@
+package com.ftn.isa22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Isa22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Isa22Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ftn/isa22/Isa22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.isa22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Isa22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
